eaa0ce6872c40ada1ffa5316a6dc4aea6ffa451b
carlasaltirana <saltirana.carla@gmail.com>
2022-07-05 22:47:42 -0300

    Setting up project and configuring connection with API

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
+import {monedas} from '../data/monedas'
+import { useEffect } from 'react'
+
+const InputSubmit = styled.input`
+    background-color:#E7460E;
+    border: none;
+    width:100%;
+    padding:10px;
+    color: #fff;
+    font-weight:700;
+    text-transform: uppercase;
+    border-radius:5px;
+    font-size:20px;
+    transition:background-color .3s ease;
+    margin-top:25px;
+
+    &:hover{
+        background-color:#A1340E;
+    }
+
+`
+
+
+
+const Formulario = () =>{
+
+    const [criptos, setCriptos] = useState([])
+
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda',monedas)
+    //const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda',monedas)
+
+    useEffect(() => {
+        const consultarApi = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=ARS"
+            const respuesta = await fetch(url)
+            const resultado= await respuesta.json()   
+           
+            
+          const arrayCriptos = resultado.Data.map(cripto =>{
+
+                console.log(cripto.coinInfo)
+                const objeto ={
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.Fullname
+                }
+                return objeto
+            })
+         
+            setCriptos(arrayCriptos)
+        }
+        consultarApi();
+    },[])
+
+    return(
+        <form>
+            
+            <SelectMonedas />
+      
+            
+
+            <InputSubmit
+                 type="submit" 
+                 value="Cotizar"/>
+
+        </form>
+
+    )
+}
+
+export default Formulario
---
+++ src/data/monedas.js
@@ -0,0 +1,12 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar Estadounidense'},
+    {id: 'ARG', nombre: 'Peso Argentino'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+    {id: 'MXN', nombre: 'Peso Mexicano'},
+    {id: 'ABC', nombre: '...'},
+    {id: 'DEF', nombre: '...'},
+    {id: 'GHI', nombre: '...'},
+]
+
+export { monedas } 
